d94f1aad45a2a5a51d8f9050945d8306ea129b35
Rickard Andersson <gonz@severnatazvezda.com>
2020-10-31 22:33:28 +0200

    add a toolbar, make it at least a bit nice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ConfigurationService } from "./services/configuration.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "creds-ui";
