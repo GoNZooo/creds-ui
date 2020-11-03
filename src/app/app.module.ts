9d2408449528136929c152caa0653111699ef63f
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-04 01:20:29 +0200

    create `MaterialModule`
    Useful for encapsulating all the material stuff in one module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { UsersService } from "./services/users.service";
-import { MatTableModule } from "@angular/material/table";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AuthorizationInterceptor } from "./interceptors/authorization.interceptor";
 import { UsersListComponent } from "./users-list/users-list.component";
@@ -19,15 +16,9 @@ import { NewUserComponent } from "./new-user/new-user.component";
 import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CopyTextComponent } from "./copy-text/copy-text.component";
 import { CommonModule } from "@angular/common";
-import { MatTooltipModule } from "@angular/material/tooltip";
 import { ClipboardModule } from "@angular/cdk/clipboard";
-import { MatButtonModule } from "@angular/material/button";
-import { MatLineModule, MatRippleModule } from "@angular/material/core";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatToolbarModule } from "@angular/material/toolbar";
 import { DeleteUserConfirmationComponent } from "./delete-user-confirmation/delete-user-confirmation.component";
-import { MatDialogModule } from "@angular/material/dialog";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
   declarations: [
@@ -45,22 +36,12 @@ import { MatDialogModule } from "@angular/material/dialog";
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
     HttpClientModule,
     Routing,
     FormsModule,
     ReactiveFormsModule,
-    MatTooltipModule,
     ClipboardModule,
-    MatButtonModule,
-    MatRippleModule,
-    MatIconModule,
-    MatInputModule,
-    MatLineModule,
-    MatToolbarModule,
-    MatDialogModule,
+    MaterialModule,
   ],
   providers: [
     UsersService,
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatRippleModule } from "@angular/material/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatDialogModule } from "@angular/material/dialog";
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatInputModule,
+  MatTooltipModule,
+  MatRippleModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatToolbarModule,
+  MatDialogModule,
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
